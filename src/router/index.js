5a939335278634b7c386c58446bfda45120d7fd1
tim <nagisa12164@163.com>
2025-01-18 21:51:25 +0800

    feat: update hash page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import ResumeForm from '@/views/ResumeForm.vue'
 import CreateResume from '@/views/CreateResume.vue'
@@ -23,7 +23,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
