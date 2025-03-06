fe8a9edef5af615514a078721f0f13db9a3c0192
tim <nagisa12164@163.com>
2025-03-06 19:55:09 +0800

    feat: update pages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/ai-cv/' 
+    // ? '/ai-cv/' 
+    ? '/' 
     : '/',
   transpileDependencies: true,
   configureWebpack: {
