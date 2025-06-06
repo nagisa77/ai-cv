12ab98f7840f7293c56f35060c82976dedfdd38e
tim <nagisa12164@163.com>
2025-06-06 13:34:53 +0800

    feat: update pages

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,9 +4,12 @@ import axios from 'axios'
 // 创建带基础配置的实例
 const apiClient = axios.create({
   // baseURL: 'http://localhost:9000',
-  // baseURL: 'http:////192.168.7.235:9000',
-  // baseURL: 'https://23.159.248.46:9000',
-  baseURL: 'https://1307107697-5q335hkuc1.ap-guangzhou.tencentscf.com',
+
+  // guangzhou 
+  // baseURL: 'https://1307107697-5q335hkuc1.ap-guangzhou.tencentscf.com',
+
+  // hongkong
+  baseURL: 'https://1307107697-31bxe8nt1v.ap-hongkong.tencentscf.com',
   headers: {
     'Content-Type': 'application/json'
   },
