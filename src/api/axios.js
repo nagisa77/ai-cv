fe3d3e8b42f5401ce3db6b69b27352d76e652553
tim <nagisa12164@163.com>
2025-09-09 23:17:17 +0800

    fix: 对接个人帐号

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,10 +8,10 @@ const apiClient = axios.create({
   // guangzhou 
   // baseURL: 'https://1307107697-5q335hkuc1.ap-guangzhou.tencentscf.com',
   // hongkong 个人账号
-  // baseURL: 'https://1307107697-31bxe8nt1v.ap-hongkong.tencentscf.com',
+  baseURL: 'https://1307107697-31bxe8nt1v.ap-hongkong.tencentscf.com',
 
   // hongkong 企业账号
-  baseURL: 'https://1352727507-8325cs7au7.ap-hongkong.tencentscf.com',
+  // baseURL: 'https://1352727507-8325cs7au7.ap-hongkong.tencentscf.com',
   headers: {
     'Content-Type': 'application/json'
   },
