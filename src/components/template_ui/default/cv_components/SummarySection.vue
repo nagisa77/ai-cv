e6d7741d04e8a7cdc4c19f03e6c88a6da21dd313
肖航 <1513314768@qq.com>
2025-09-22 11:11:29 +0800

    完成默认模板的修改字体大小

--- src/components/template_ui/default/cv_components/SummarySection.vue
+++ src/components/template_ui/default/cv_components/SummarySection.vue
@@ -1,7 +1,7 @@
 <!-- src/components/SummarySection.vue -->
 <template>
     <section v-if="personalSummary" class="summary-section session">
-      <h2 class="session-title">个人总结</h2>
+      <div class="session-title">个人总结</div>
       <div class="item-summary">{{ personalSummary }}</div>
     </section>
   </template>
@@ -19,10 +19,6 @@
   </script>
   
   <style scoped>
-  .summary-section p {
-    font-size: 9px;
-  }
-  
   .summary-section {
     display: flow-root;
   }
