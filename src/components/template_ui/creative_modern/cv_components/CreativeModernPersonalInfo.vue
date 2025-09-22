28e52e05b29cf3916f5dfafc6b283786bdeb13fa
肖航 <1513314768@qq.com>
2025-09-22 12:52:54 +0800

    完成创意模板修改字体大小功能的实现

--- src/components/template_ui/creative_modern/cv_components/CreativeModernPersonalInfo.vue
+++ src/components/template_ui/creative_modern/cv_components/CreativeModernPersonalInfo.vue
@@ -3,18 +3,18 @@
   <div class="personal-info">
     <img v-if="personalInfo.avatar" :src="personalInfo.avatar" alt="头像" class="avatar">
     <div class="info-session">
-      <h1 class="name" v-if="personalInfo.name">{{ personalInfo.name }}</h1>
-      <h1 class="name" v-else>（您的名称）</h1>
-      <p class="email-phone" v-if="personalInfo.email && personalInfo.phone">
+      <div class="name" v-if="personalInfo.name">{{ personalInfo.name }}</div>
+      <div class="name" v-else>（您的名称）</div>
+      <div class="email-phone" v-if="personalInfo.email && personalInfo.phone">
         {{ personalInfo.email }} | {{ personalInfo.phone }}
-      </p>
-      <p class="email-phone" v-else-if="personalInfo.email">
+      </div>
+      <div class="email-phone" v-else-if="personalInfo.email">
         {{ personalInfo.email }}
-      </p>
-      <p class="email-phone" v-else-if="personalInfo.phone">
+      </div>
+      <div class="email-phone" v-else-if="personalInfo.phone">
         {{ personalInfo.phone }}
-      </p>
-      <p class="email-phone" v-else>（您的联系方式）</p>
+      </div>
+      <div class="email-phone" v-else>（您的联系方式）</div>
     </div>
 
 
@@ -40,24 +40,11 @@ export default {
   align-items: center;
 }
 
-.name {
-  font-size: 20px;
-  font-weight: bold;
-  margin-bottom: 2px; /* 减小名字下方的间距 */
-  line-height: 1.1; /* 减小行高 */
-}
-
 .info-session {
   display: flex;
   flex-direction: column;
   align-items: center;
   padding-top: 0px;
-}
-
-.email-phone {
-  font-size: 12px;
-  margin-top: 0; /* 移除上方间距 */
-  line-height: 1; /* 减小行高 */
 }
 
 .avatar {
--- src/components/template_ui/creative_modern/cv_components/CreativeModernSummarySection.vue
+++ src/components/template_ui/creative_modern/cv_components/CreativeModernSummarySection.vue
@@ -1,8 +1,8 @@
 <!-- src/components/SummarySection.vue -->
 <template>
     <section v-if="personalSummary" class="summary-section session">
-      <h2 class="session-title">个人总结</h2>
-      <p>{{ personalSummary }}</p>
+      <div class="session-title">个人总结</div>
+      <div class="item-summary">{{ personalSummary }}</div>
     </section>
   </template>
   
@@ -17,13 +17,6 @@
     }
   };
   </script>
-  
-  <style scoped>
-  .summary-section p {
-    font-size: 9px;
-  }
-  </style>
-  
   <style scoped>
   .summary-section {
     display: flow-root;
